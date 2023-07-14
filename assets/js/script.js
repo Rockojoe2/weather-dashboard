76434d98c613dd9c50345454bbce9679ae0fa5fa
Rockojoe2 <lee.d.joey@gmail.com>
2023-07-14 00:32:56 -0500

    Added weather descriptions

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,27 +5,45 @@ var listHistory = document.getElementById("list");
 
 //Current Variables
 var currentDate = document.getElementById("current-date");
+var currentDescription = document.getElementById("current-description");
 var currentTemp = document.getElementById("current-temp");
 var currentWind = document.getElementById("current-wind");
 var currentHumidity = document.getElementById("current-humidity");
 
 //Card one variables
 var cardOneDate = document.getElementById("card-one-date");
+var cardOneDescription = document.getElementById("card-one-description");
 var cardOneTemp = document.getElementById("card-one-temp");
 var cardOneWind = document.getElementById("card-one-wind");
 var cardOneHumidity = document.getElementById("card-one-humidity");
 
 //Card Two variables
 var cardTwoDate = document.getElementById("card-two-date");
+var cardTwoDescription = document.getElementById("card-two-description");
 var cardTwoTemp = document.getElementById("card-two-temp");
 var cardTwoWind = document.getElementById("card-two-wind");
 var cardTwoHumidity = document.getElementById("card-two-humidity");
 
 //Card Three Variables
 var cardThreeDate = document.getElementById("card-three-date");
+var cardThreeDescription = document.getElementById("card-three-description");
 var cardThreeTemp = document.getElementById("card-three-temp");
 var cardThreeWind = document.getElementById("card-three-wind");
 var cardThreeHumidity = document.getElementById("card-three-humidity");
+
+//Card four Variables
+var cardFourDate = document.getElementById("card-four-date");
+var cardFourDescription = document.getElementById("card-four-description");
+var cardFourTemp = document.getElementById("card-four-temp");
+var cardFourWind = document.getElementById("card-four-wind");
+var cardFourHumidity = document.getElementById("card-four-humidity");
+
+//Card five Variables
+var cardFiveDate = document.getElementById("card-five-date");
+var cardFiveDescription = document.getElementById("card-five-description");
+var cardFiveTemp = document.getElementById("card-five-temp");
+var cardFiveWind = document.getElementById("card-five-wind");
+var cardFiveHumidity = document.getElementById("card-five-humidity");
 
 
 
@@ -74,6 +92,7 @@ listHistory.addEventListener("click", function(event) {
         console.log("This is the lon: " + lon);
         // console.log("This is the temperature: " + temperature);
         getCoordinates(lon, lat);
+        getFutureDays(lat, lon);
         
         
     })
@@ -140,14 +159,22 @@ function cityHistory() {
         var temperature = (data.list[0].main.temp);
         var wind = (data.list[0].wind.speed);
         var humidity = (data.list[0].main.humidity);
+        var description = (data.list[0].weather[0].main);
 
         currentDate.innerText = "Date: " + date;
         currentTemp.innerText = "Temp: " + temperature;
         currentWind.innerText = "Wind: " + wind;
         currentHumidity.innerText = "Humidity: " + humidity;
+
+        console.log("Description is: " + description);
+
+        if(description == "Clouds")
+        {
+          currentDescription.innerText = "☁️";
+        }
        
 
-        // console.log("This is the temperature: " + temperature);
+        //console.log("This is the temperature: " + temperature);
         // console.log("This is the wind speed: " + wind);
         // console.log("This is the humidity: " + humidity);
 
@@ -172,14 +199,13 @@ function cityHistory() {
         var wind = (data.list[10].wind.speed);
         var humidity = (data.list[10].main.humidity);
 
+
        
        
        cardOneDate.innerText = "Date: " + date;
        cardOneTemp.innerText = "Temperature: " + temperature;
        cardOneWind.innerText = "Wind: " + wind;
        cardOneHumidity.innerText = "Humidity: " + humidity;
-
-
 
     })
 
@@ -225,14 +251,54 @@ function cityHistory() {
 
     })
 
+        //Day 4
+        fetch(cityAPI)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data){
+    
+            var date = (data.list[34].dt_txt);
+            var temperature = (data.list[34].main.temp);
+            var wind = (data.list[34].wind.speed);
+            var humidity = (data.list[34].main.humidity);
+    
+           
+           
+           cardFourDate.innerText = "Date: " + date;
+           cardFourTemp.innerText = "Temperature: " + temperature;
+           cardFourWind.innerText = "Wind: " + wind;
+           cardFourHumidity.innerText = "Humidity: " + humidity;
+    
+        })
+
+                //Day 5
+                fetch(cityAPI)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data){
+            
+                    var date = (data.list[34].dt_txt);
+                    var temperature = (data.list[34].main.temp);
+                    var wind = (data.list[34].wind.speed);
+                    var humidity = (data.list[34].main.humidity);
+            
+                   
+                   
+                   cardFiveDate.innerText = "Date: " + date;
+                   cardFiveTemp.innerText = "Temperature: " + temperature;
+                   cardFiveWind.innerText = "Wind: " + wind;
+                   cardFiveHumidity.innerText = "Humidity: " + humidity;
+            
+                })
+
 
 
 
 
 
   }
-
- 
 
 cityHistory();
 // savedCity();
